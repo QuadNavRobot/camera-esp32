1cc132a6a6af84126464caa06c16441fa56d0fbb
Fabio Gazzoni <fabio.gazzoni@mi.unc.edu.ar>
2025-01-16 09:43:48 -0300

    refactor code and JPEG image format

--- main/inc/main.h
+++ main/inc/main.h
@@ -2,6 +2,7 @@
 #include <nvs_flash.h>
 #include <sys/param.h>
 #include <string.h>
+#include "esp_heap_caps.h"
 
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
@@ -9,12 +10,14 @@
 #include <uros_network_interfaces.h>
 #include <sensor_msgs/msg/image.h>
 #include <micro_ros_utilities/string_utilities.h>
+#include <sensor_msgs/msg/compressed_image.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-#include "esp_camera.h"
+#include "../inc/camera.h"
+// #include "../inc/storage.h"
 
 // Micro-ROS definitions
 #ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
@@ -23,32 +26,11 @@
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
-
-// ESP32Cam (AiThinker) PIN Map
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1 // software reset will be performed
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
 void FreeRTOS_Init();
 void init_microROS();
 void TaskTakePicture(void *argument);
 void TaskSendPicture(void *argument);
 
-QueueHandle_t pictureQueue;
 rcl_node_t esp32_cam_node;
 
 rclc_support_t support;
